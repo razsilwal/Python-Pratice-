a7546aa41d024ead6e259bfd6a51eadd199742ac
razsilwal <sabalsilwal51@gmail.com>
2025-07-02 20:01:36 +0545

    learning array

---
+++ Day3.py
@@ -0,0 +1,28 @@
+# # name = ----My ----sister name is sujata @@ pathak 1  2  3 __ 
+# # result = Sujata Pathak
+# '''
+# split()
+# capitalize()
+# title()
+
+# '''
+
+# name = " ----sujata @@ pathak 1  2  3 __"
+# new_name = name.strip(" -123_ ")
+# print(new_name)
+
+# new_name1 = new_name.replace(" @@", "")
+# print(new_name1)
+
+# new_name_final = new_name1.title()
+# print(new_name_final)
+
+# first_name, last_name = new_name_final.split() 
+# print(first_name)
+# print(last_name)
+
+
+# ph_no = "(+977)9865247946"
+# ph_no1 = ph_no.replace("(+977)","")
+# ph_no2 = 
+
---
+++ Day4.py
@@ -0,0 +1,12 @@
+
+l3 = ["Raj", "Ram", 32, True, False, 3.2, 4.1, "Ramu", "Shyam", "Red Bull", 3.5, "Gita", "TensorFlow", "NumPy"]
+
+# slicing 
+
+print(len(l3))
+print(l3[-1]) #  print the last element
+print(l3[1])
+
+print(l3[-14:3])
+# print(l3[1:8])
+# print(l3[7:9])
