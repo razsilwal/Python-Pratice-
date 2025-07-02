85f434ade31925fe9f0761eedf589e579af65ea1
razsilwal <sabalsilwal51@gmail.com>
2025-07-02 21:28:13 +0545

    learn about list, tuple, set and Dict

--- Day4.py
+++ Day4.py
@@ -1,12 +1,72 @@
 
-l3 = ["Raj", "Ram", 32, True, False, 3.2, 4.1, "Ramu", "Shyam", "Red Bull", 3.5, "Gita", "TensorFlow", "NumPy"]
-
+# l3 = ["Raj", "Ram", 32, True, False, 3.2, 4.1, "Ramu", "Shyam", "Red Bull", 3.5, "Gita", "TensorFlow", "NumPy"]
 # slicing 
 
-print(len(l3))
-print(l3[-1]) #  print the last element
-print(l3[1])
+# print(len(l3))
+# print(l3[-1]) #  print the last element
+# print(l3[1])
+# print(l3[::-1])
+# print(l3[-14:3])
 
-print(l3[-14:3])
 # print(l3[1:8])
 # print(l3[7:9])
+
+# l2 = ["Ram", "Shyam", 1, 2, 3, True, "Ram"]
+
+# l2.append("banana")
+# print(l2)
+
+# l2.insert(0, "Krishna") # it insert the element
+# print(l2)
+
+# my_list = [33,65,12,78,25,65,84,45,0,2,5]
+# print(my_list)
+# my_list.sort() # in ascending order
+# print(my_list)
+# my_list.sort(reverse=True) # sort in descending order
+# print(my_list)
+# my_list.pop()
+# print(my_list)
+
+# my_list.remove(12)
+# print(my_list)
+
+# my_list.clear()
+# print(my_list)
+
+
+
+
+# tuple
+
+# my_tuple = (1,4,3,5,6,True, "Ram", False, 1,0,1,0)
+# print(my_tuple)
+# print(type(my_tuple))
+
+# counting = my_tuple.count(1)
+# print(counting)
+
+# inde9 = my_tuple.index(5)
+# print(inde9)
+
+
+# SET
+
+# my_set = {1,5,4,3,5,4,2}
+# print(my_set)
+# print(type(my_set))
+
+# my_set.add(7)
+# print(my_set)
+
+# my_set.add(-1)
+# print(my_set)
+
+
+# Dictionery
+
+my_set = {"Name":"Ram", "Age":20, "ph_no":9865247946}
+print(type(my_set))
+print(my_set)
+print(my_set.keys())
+print(my_set.values())
